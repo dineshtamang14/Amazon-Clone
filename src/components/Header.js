ba7b050af22e3c25551e44c83b7ed4bdc01752c1
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-28 17:35:29 +0530

    added sreach

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import Image from "next/image";
+import {
+    MenuIcon,
+    SearchIcon,
+    ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
     return (
         <header>
             {/* top header  */}
-            <div className="flex items-center bg-amazon_blue">
-                <div>
+            <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+                <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
                     <Image 
                         src="https://links.papareact.com/f90"
                         width={150}
@@ -14,6 +19,13 @@ function Header() {
                         className="cursor-pointer"
                     />
                 </div>
+
+                {/* search */}
+                <div className="hidden sm:flex item-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+                    <input className="p-2 h-full w-6 flex-grow" type="text" />
+                    <SearchIcon className="h-12 p-4"/>
+                </div>
+
             </div>
 
             {/* bottom header */}
