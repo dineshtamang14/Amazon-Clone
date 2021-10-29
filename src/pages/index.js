735f809f8ee6a092cb1c3d13b021e20f372586de
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-29 17:16:42 +0530

    bottom nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,23 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner"
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon Clone</title>
       </Head>
 
       {/* Header */}
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+
+        {/* Product Feed */}
+      </main>
 
     </div>
   );
