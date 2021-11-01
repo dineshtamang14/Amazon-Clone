4ec33e2ec9de2bd36b93c87f8acb75c20f2d1ed0
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-01 17:11:34 +0530

    added the products

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,9 +1,22 @@
-function ProductFeed() {
-    return (
-        <div>
-            
-        </div>
-    )
+import Product from "./Product";
+
+function ProductFeed({ products }) {
+  return (
+    <div>
+      <h1>Products here...</h1>
+      {products.map(({id, title, price, description, category, image}) => (
+        <Product
+          key={id}
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ProductFeed;
