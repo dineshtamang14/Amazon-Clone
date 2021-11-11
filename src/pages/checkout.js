5125c24b6fd20ca252087f85bbdf070531f82992
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-11 17:23:31 +0530

    aded the next router

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,7 +1,9 @@
+import Header from "../components/Header";
+
 function Checkout() {
     return (
-        <div>
-            
+        <div className="bg-gray-100">
+            <Header />
         </div>
     )
 }
