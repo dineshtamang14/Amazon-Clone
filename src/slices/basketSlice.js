bb678fc425a4f57e4f740276a71f0e1213ebb7ec
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-11 18:00:41 +0530

    added product to redux store

--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -9,7 +9,9 @@ export const basketSlice = createSlice({
   initialState,
   // Actions
   reducers: {
-    addToBasket: (state, action) => {},
+    addToBasket: (state, action) => {
+      state.items = [...state.items, action.payload]
+    },
     removeFromBasket: (state, action) => {},
   },
 });
