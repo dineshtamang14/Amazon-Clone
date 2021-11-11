9cfce5563792791d404cfb400e07bbb468cc2675
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-11 16:35:02 +0530

    added the auth

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDqy5AlBRaKSwlrxdP9DQV2zSWm_v4vcFE",
+  authDomain: "clone-2a092.firebaseapp.com",
+  projectId: "clone-2a092",
+  storageBucket: "clone-2a092.appspot.com",
+  messagingSenderId: "745902656829",
+  appId: "1:745902656829:web:683b1d2e95e647c60b4954",
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -55,7 +55,7 @@ function Header() {
         {/* bottom header */}
         <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
           <p className="link flex items-center">
-            <MenuIcon className="h-6 mr-1" />
+            <MenuIcon className="h-7 mr-1" />
             All
           </p>
           <p className="link"> Prime Video</p>
