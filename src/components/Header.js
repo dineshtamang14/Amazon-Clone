963c3b38d0a437b105c66068ebf4f170ed6f8a2a
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-11 17:15:20 +0530

    added next auth js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,8 @@ import {
 import { signIn, signOut, useSession } from "next-auth/client";
 
 function Header() {
+  const [session] = useSession();
+
     return (
       <header>
         {/* top header  */}
@@ -32,11 +34,13 @@ function Header() {
 
           {/* Right */}
           <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-            <div onClick={signIn} className="link">
-              <p>Hello Dinesh</p>
+            <div onClick={!session ? signIn : signOut} className="link">
+              <p className="hover:underline">
+                {session ? `Hello, ${session.user.name}` : "Sign In"}
+              </p>
               <p className="font-extrabold md:text-sm">Account & Lists</p>
             </div>
-            <div className="link">
+            <div className="cursor-pointer link">
               <p>Returns</p>
               <p className="font-extrabold md:text-sm">& Orders</p>
             </div>
