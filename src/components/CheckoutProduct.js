a17decca5d91046fe786a44a485110e8c0eb072b
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-12 11:38:48 +0530

    added add to basket features

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,3 +1,6 @@
+import { StarIcon } from "@heroicons/react/outline";
+import Image from "next/dist/client/image";
+
 function CheckoutProduct({ 
     id, 
     title,
@@ -9,8 +12,20 @@ function CheckoutProduct({
     hasPrime,
  }) {
     return (
-        <div>
-            
+        <div className="grid grid-cols-5">
+            <Image src={image} height={200} width={200} objectFit="contain" />
+
+            {/* middle */}
+            <div className="col-span-3 mx-5">
+                <p>{title}</p>
+                <div className="flex">
+                    {Array(rating).fill().map((_, i)=>(
+                        <StarIcon key={i} className="h-5 text-yellow-500" />
+                    ))}
+                </div>
+
+                <p className="text-xs">{description}</p>
+            </div>
         </div>
     )
 }
