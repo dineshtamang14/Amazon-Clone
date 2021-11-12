a91d64702c9a4e01fc55ef2ec92f9caf0daa059d
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-12 11:25:16 +0530

    added the checkout product

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import Image from "next/dist/client/image";
 import { useSelector } from "react-redux";
 import { selectItems } from "../slices/basketSlice";
+import CheckoutProduct from "../components/CheckoutProduct";
  
 function Checkout() {
   const items = useSelector(selectItems);
@@ -21,10 +22,12 @@ function Checkout() {
             />
 
             <div className="flex flex-col p-5 space-y-10 bg-white">
-                <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+                <h1 className="text-3xl border-b pb-4">{items.length === 0 ? "Your Amazon Basket is Empty." : "Shopping Basket."}</h1>
+                {items.map((item, i)=> (
+                  <CheckoutProduct />
+                ))}
             </div>
           </div>
-
           {/* Right */}
             <div>
 
